ce1d2fec5228384707bc61815ee2e8af44ebdf7b
mahao <928434248@qq.com>
2024-07-16 10:38:24 +0800

    add update

---
+++ hardhat.config.ts
@@ -0,0 +1,16 @@
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import '@nomicfoundation/hardhat-foundry'
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.24',
+  typechain: {
+    outDir: './typechain',
+    target: 'ethers-v6',
+    alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+    externalArtifacts: ['externalArtifacts/*.json'], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+    dontOverrideCompile: false, // defaults to false
+  },
+}
+
+export default config
