4ab3db4d4d7ffe59c97a7c392a86e1162316085a
Junda Liu <junda@celer.network>
2024-07-17 16:22:41 -0700

    update config and deploy latest hook impl

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.24;
 import "forge-std/Script.sol";
 import "forge-std/StdJson.sol";
 
-import {BrevisFee} from "../src/BrevisFeeVault.sol";
+// import {BrevisFee} from "../src/BrevisFeeVault.sol";
 import {BinVipHook,IBinPoolManager} from "../src/pool-bin/BinVipHook.sol";
 import {CLVipHook,ICLPoolManager} from "../src/pool-cl/CLVipHook.sol";
 
@@ -26,8 +26,8 @@ contract Deploy is Script {
 
         vm.startBroadcast();
 
-        BrevisFee feev = new BrevisFee();
-        console.log("BrevisFee contract deployed at ", address(feev));
+        //BrevisFee feev = new BrevisFee();
+        //console.log("BrevisFee contract deployed at ", address(feev));
 
         BinVipHook binHook = new BinVipHook(IBinPoolManager(binpm), 0, brevReq);
         console.log("binHook contract deployed at ", address(binHook));
