0369d5ed421ac4b3d14a0256d5ce87c90ab80dd2
Junda Liu <junda@celer.network>
2024-12-12 21:59:50 -0800

    vkhash

--- script/SetVkHash.sol
+++ script/SetVkHash.sol
@@ -8,7 +8,7 @@ import {BinVipHook} from "../src/pool-bin/BinVipHook.sol";
 contract Deploy is Script {
     function run() public {
         address hookAddr = vm.envAddress("HOOK_ADDR");
-        bytes32 vkhash = 0x0fe1d819796cb0ebb1a30a613e8cd425619c0d73e52d62dc2298435994f9927a;
+        bytes32 vkhash = 0x00abe8e8da6b17cdf3dc6709562f39a4f6df9e54738203325a9aad293b7f3eb3;
         BinVipHook binHook = BinVipHook(hookAddr);
         
         vm.startBroadcast();
